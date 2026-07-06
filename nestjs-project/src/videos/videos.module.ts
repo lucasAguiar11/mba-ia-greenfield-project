07a71387eb3d6ada15ea1aa59d4f2e78469f3341
Lucas Aguiar <lucas99.abreu@outlook.com>
2026-07-06 10:47:32 -0300

    feat(phase-03): registrar storage, fila e módulo de vídeos no AppModule

---
+++ nestjs-project/src/videos/videos.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Video } from './entities/video.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Video])],
+})
+export class VideosModule {}
